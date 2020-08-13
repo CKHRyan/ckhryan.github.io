0c807fee52436586914e2bc1e3c840ff0d41deab
Ryan Chung Ka Ho <rchungkh@nexify.com.hk>
2020-08-13 22:55:10 +0800

    update for food spreadsheet page

---
+++ 7110eda4d09e062aa5e4a390b0a572ac0d2c0220/foodtable.js
@@ -0,0 +1,93 @@
+
+var restaurant_obj;
+
+window.onload = function() {
+	var width = document.getElementById("main").getBoundingClientRect().width;
+	adjustStyle(width);
+	console.log(window.location.pathname);
+	if (window.location.pathname.includes("index.html")) {
+		updateDate();
+	}
+	loadExcel();
+}
+
+function adjustStyle(width) {
+	console.log(width);
+	if (width < 750) {
+		console.log("mobile");
+		$("#size-stylesheet").attr("href", "bodiary_mb.css");
+	} else {
+		console.log("device");
+		$("#size-stylesheet").attr("href", "bodiary_pc.css"); 
+	}
+}
+	
+
+function loadExcel() {
+	$.ajax({
+	  url: "https://script.google.com/macros/s/AKfycbze1j2R4y_jffgPxLinwx7gJPMOMsBLq2Vlq5vqtP-kxo-Xd0M/exec",
+	  type: "GET",
+	  dataType: "json",
+	  async: true,
+		beforeSend: function(x) {
+			if(x && x.overrideMimeType) {
+			   x.overrideMimeType("application/j-son;charset=UTF-8");
+			}
+			console.log('Request sent.');
+		},
+	  success: function(Jdata) {
+		console.log(Jdata);
+		restaurant_obj = Jdata;
+		document.getElementById("foodtable_loadbar").style.display = "none";
+		document.getElementById("foodtable_loadspinner").style.display = "none";
+		document.getElementById("foodtable_option").style.display = "block";
+	  },
+	  
+	  error: function() {
+		alert("ERROR!!!");
+	  }
+	});
+	
+}
+
+function printFoodTable(region) {
+	
+	if (restaurant_obj == null)
+		return;
+
+	var ft = document.getElementById('foodtable').getElementsByTagName('table')[0];
+	while(ft.hasChildNodes())
+	{
+	   ft.removeChild(ft.firstChild);
+	}
+	
+	var district_rt_obj;
+	switch(region) {
+		case "HK":
+			district_rt_obj = restaurant_obj["Hong Kong Island"];
+			break;
+		case "KL":
+			district_rt_obj = restaurant_obj["Kowloon"];
+			break;
+		case "NT":
+			district_rt_obj = restaurant_obj["New Territories"];
+			break;
+		default:
+			console.log("The region is not valid");
+			return;
+	}
+
+	for (var dt in district_rt_obj) {
+		var row = ft.insertRow(ft.rows.length);
+		row.style = "background-color: #15acee; line-height: 30px;";
+		row.insertCell(0).innerHTML = dt;
+		for (var rt in district_rt_obj[dt]) {
+			var row = ft.insertRow(ft.rows.length);
+			row.style = "background-color: #d3e9fe; line-height: 25px;";
+			row.insertCell(0).innerHTML = district_rt_obj[dt][rt].Name;
+			row.insertCell(1).innerHTML = district_rt_obj[dt][rt].Address;
+			row.insertCell(2).innerHTML = district_rt_obj[dt][rt].Cuisine;
+			row.insertCell(3).innerHTML = district_rt_obj[dt][rt].PriceRange;
+		}
+	} 
+}
