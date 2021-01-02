f3632b74e7b909c9729e4a1e808209fcc471ed5c
CKHRyan <1155125428@link.cuhk.edu.hk>
2021-01-02 20:52:57 +0800

    Upload files of personal webpage

--- 7110eda4d09e062aa5e4a390b0a572ac0d2c0220/global.js
+++
@@ -1,4 +0,0 @@
-if (!window.location.href.substring(0, 8).includes("https://") && !window.location.href.substring(0, 7).includes("http://")) {
-	alert("The web pages can be viewed until uploaded to the Internet");
-	//window.stop();
-}
